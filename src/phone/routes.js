9eb0d7913a68ed2a2d9535acb9a95ed7bf97a4cf
etfaghaoubeid <etfaghaoubeid@gmail.com>
2021-04-27 12:46:21 +0000

    add route for get one phone

--- src/phone/routes.js
+++ src/phone/routes.js
@@ -1,10 +1,17 @@
 const { Router } = require("express");
-const { getPhones, addPhone, deletePhone, editPhone } = require("./controller");
+const {
+  getPhones,
+  addPhone,
+  deletePhone,
+  editPhone,
+  getPhone,
+} = require("./controller");
 const router = Router();
 
 router.get("/", getPhones);
 router.post("/add-phone", addPhone);
 router.delete("/phone/:id", deletePhone);
+router.get("/phone/:id", getPhone);
 router.put("/phone-update/:id", editPhone);
 
 module.exports = router;
